ecea59691eb978dcf724067bbe449d6360f6d6ff
WeinbergerPeti <weinbergerpeti@gmail.com>
2024-01-12 16:28:35 +0100

    1. módosítás

--- database/migrations/2024_01_12_152006_create_tabla_nevs_table.php
+++ database/migrations/2024_01_12_152006_create_tabla_nevs_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('tabla_nevs', function (Blueprint $table) {
             $table->id();
+            $table->string("szoveg");
             $table->timestamps();
         });
     }
